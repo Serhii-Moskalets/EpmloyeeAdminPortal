4f6bfa197af4765949c72dfb60547507de353c33
Serhii Moskalets <s.mosklaets16@gmail.com>
2025-09-05 11:08:11 +0200

    Добавьте файлы проекта.

---
+++ EpmloyeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,90 @@
+﻿using EpmloyeeAdminPortal.Data;
+using EpmloyeeAdminPortal.Models;
+using EpmloyeeAdminPortal.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EpmloyeeAdminPortal.Controllers
+{
+    // localhost:xxxx/api/
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllEmployees() => Ok(_context.Employess.ToList());
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id)
+        {
+            var employee = _context.Employess.Find(id);
+
+            if (employee is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary,
+            };
+
+            _context.Employess.Add(employeeEntity);
+            _context.SaveChanges();
+
+            return Ok(employeeEntity);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateEmployee(Guid id, UpdateEmployeeDto updateEmployeeDto)
+        {
+            var employee = _context.Employess.Find(id);
+
+            if (employee is null)
+            {
+                return NotFound();
+            }
+
+            employee.Name = updateEmployeeDto.Name;
+            employee.Email = updateEmployeeDto.Email;
+            employee.Phone = updateEmployeeDto.Phone;
+            employee.Salary = updateEmployeeDto.Salary;
+
+            _context.SaveChanges();
+
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            var employee = _context.Employess.Find(id);
+
+            if (employee is null)
+            {
+                return NotFound();
+            }
+
+            _context.Employess.Remove(employee);
+            _context.SaveChanges();
+
+            return Ok(employee);
+        }
+    }
+}
---
+++ EpmloyeeAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EpmloyeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EpmloyeeAdminPortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employess { get; set; } = null!;
+
+
+    }
+}
